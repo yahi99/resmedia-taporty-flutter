bcc75e3f5671dcb3b8644727e1e3b76a1097e4b2
Emilio Dalla Torre <emiliodallatorre12@live.com>
2019-09-08 15:02:55 +0200

    Sistemata tutta la questione dei plugin.

--- app/ios/Flutter/flutter_export_environment.sh
+++ app/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/vicino/Documents/flutter_files/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/vicino/Documents/app/Projects/resmedia-taporty-flutter"
+export "FLUTTER_ROOT=/home/emiliodallatorre/Documenti/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/emiliodallatorre/Documenti/GitHub/resmedia-taporty-flutter/app"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/vicino/Documents/flutter_files/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_FRAMEWORK_DIR=/home/emiliodallatorre/Documenti/Flutter/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"

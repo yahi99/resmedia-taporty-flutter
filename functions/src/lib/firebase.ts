220d10a590a5bffe6c368a12b1fffcdba063e252
Freddy933 <noncisonousernameliberixd@gmail.it>
2019-09-09 13:46:28 +0200

    Messe coordinate e TODO

--- functions/src/lib/firebase.ts
+++
@@ -1,8 +0,0 @@
-import * as admin from 'firebase-admin'
-
-// tslint:disable-next-line: no-use-before-declare
-export { app, fs, sms }
-
-const app = admin.initializeApp()
-const fs = admin.firestore(app)
-const sms = admin.messaging(app)
--- lib/data/config.dart
+++
@@ -1,7 +0,0 @@
-const SPACE = 12.0;
-
-class Cfg {
-  static const double LOGO_SPACE = 40.0;
-
-
-}

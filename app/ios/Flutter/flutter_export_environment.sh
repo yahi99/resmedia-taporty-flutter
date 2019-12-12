848c7fb4340417cf29cf1b76a2f7de11d036c3e8
Emilio Dalla Torre <emiliodallatorre12@live.com>
2019-12-12 14:56:27 +0100

    Rivisti completamente tutti i plugin e le versioni nel pubspec.

--- app/ios/Flutter/flutter_export_environment.sh
+++ app/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/giorgio/res_media/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/giorgio/AndroidStudioProjects/resmedia-taporty-flutter/app"
+export "FLUTTER_ROOT=/home/emiliodallatorre/Documenti/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/emiliodallatorre/Documenti/GitHub/resmedia-taporty-flutter/app"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/giorgio/res_media/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/emiliodallatorre/Documenti/Flutter/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.3"
 export "FLUTTER_BUILD_NUMBER=4"
